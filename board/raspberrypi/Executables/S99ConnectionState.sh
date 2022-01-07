87eafd3f31376b909157f2cda414803e07733492
d3structor84 <a.w84@t-online.de>
2022-01-07 23:38:03 +0100

    Update S99ConnectionState.sh

--- board/raspberrypi/Executables/S99ConnectionState.sh
+++ board/raspberrypi/Executables/S99ConnectionState.sh
@@ -1,37 +1,45 @@
 #!/bin/sh
-TIMEOUT=30
-counter=0
-connection=0
 
-until [ $counter -eq $TIMEOUT ]
-do
+case "$1" in
+    start|"")
+		TIMEOUT=20
+		counter=0
+		connection=0
 
-        if echo $(iwconfig 2>/dev/null) | grep -q "ESSID:off"; then
-                sleep 1
-                counter=$((counter+1))
-                connection=0
-        else
-                connection=1
-                break
-        fi
-done
+		until [ $counter -eq $TIMEOUT ]
+		do
+			if echo $(iwconfig 2>/dev/null) | grep -q "ESSID:off"; then
+				sleep 1
+				counter=$((counter+1))
+				connection=0
+			else
+				connection=1
+				break
+			fi
+		done
 
-if [ $connection -eq 0  ]; then
-        echo none > /sys/class/leds/led0/trigger
+		if [ $connection -eq 0 ]; then
+			echo none > /sys/class/leds/led0/trigger
 
-        echo 1 > /sys/class/leds/led0/brightness
-        sleep 0.5
-        echo 0 > /sys/class/leds/led0/brightness
-        sleep 0.5
-        echo 1 > /sys/class/leds/led0/brightness
-        sleep 0.5
-        echo 0 > /sys/class/leds/led0/brightness
-        sleep 0.5
-        echo 1 > /sys/class/leds/led0/brightness
-        sleep 0.5
-        echo 0 > /sys/class/leds/led0/brightness
-        sleep 0.5
+			echo 1 > /sys/class/leds/led0/brightness
+			sleep 0.5
+			echo 0 > /sys/class/leds/led0/brightness
+			sleep 0.5
+			echo 1 > /sys/class/leds/led0/brightness
+			sleep 0.5
+			echo 0 > /sys/class/leds/led0/brightness
+			sleep 0.5
+			echo 1 > /sys/class/leds/led0/brightness
+			sleep 0.5
+			echo 0 > /sys/class/leds/led0/brightness
+			sleep 0.5
 
-  /bin/sh /usr/html/Resources/Router/zrun.sh
-  reboot
-fi
+			/bin/sh /usr/html/Resources/Router/zrun.sh
+			reboot
+		fi
+	;;
+    stop) ;;
+    *)
+	exit 1
+	;;
+esac
